6f50795f99f0f2d636f5537e6b5db74825c6585a
sibananda pathy <116892811+Sivaan66@users.noreply.github.com>
2025-06-10 09:27:53 -0700

    Create TreasureHunt.py

---
+++ TreasureHunt.py
@@ -0,0 +1,20 @@
+def coordinates():
+    t = int(input())
+    for _ in range(t) :
+        moves = input().strip()
+        x=0
+        y=0
+        for move in moves:
+            if move == "U":
+                y+=1
+            elif move == "D":
+                y-=1
+            elif move == "L":
+                x-=1
+            elif move == "R":
+                x+=1
+            else:
+                print("We don't have any other directions")
+        print("(x,y) = ",x,y)
+
+coordinates()
