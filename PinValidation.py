e6e9f672c9bd2b15ec4d38c5a9d05624fc766c05
sibananda pathy <116892811+Sivaan66@users.noreply.github.com>
2025-06-15 10:12:47 -0700

    Update PinValidation.py

--- PinValidation.py
+++ PinValidation.py
@@ -1,9 +1,17 @@
 def PINVALIDATION():
     PIN = int(input("Enter the PIN :").strip())
-    if PIN % 4 == 0 :
+    pin_str = str(PIN)
+    pin_list = list(pin_str)
+    if len(pin_list) != 4 :
+        print("Wrong PIN")
+        print(PINVALIDATION())
+        return
+    pin = int("".join(pin_list))
+    if pin % 4 == 0 :
         print("OPEN")
     else:
         print("LOCKED")
         print(PINVALIDATION())
+        
 
 PINVALIDATION()
