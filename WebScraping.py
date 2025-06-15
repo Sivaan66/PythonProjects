7fb8bf31d882bc206b86db11dbe0913372eceeff
sibananda pathy <116892811+Sivaan66@users.noreply.github.com>
2025-06-15 09:53:26 -0700

    Update WebScraping.py

--- WebScraping.py
+++ WebScraping.py
@@ -7,21 +7,21 @@ page = requests.get(url, headers =headers)
 
 content = BeautifulSoup(page.text,'html.parser')
 table =content.find('table')
-table =content.find('table', class_ = 'wikitable sortable')
-columns = table.find_all('th')
-columns_text = [text.text.strip() for text in columns]
+table =content.find('table', class_ = 'wikitable sortable') # finding the table with class "wikitable sortable"
+columns = table.find_all('th') # selecling the column of the table finding all 'th' from "wikitable sortable
+columns_text = [text.text.strip() for text in columns] # converting the raw exported data to text format and *strip()* to remove any unnecessey spaces
 
-import pandas as pd
+import pandas as pd 
 
 df = pd.DataFrame(columns = columns_text)
 
-rows = table.find_all('tr')
+rows = table.find_all('tr') # Code to find the row data
 all_data = []
-for data in rows[1:]:
+for data in rows[1:]:                # running loop to collect the data and append it in the empty list all_data[]
     row_data = data.find_all("td")
     row_data_text = [text.text.strip() for text in row_data]
     if len(row_data_text)==len(columns_text):
         all_data.append(row_data_text)
 
-df = pd.DataFrame(all_data,columns = columns_text)
-df.to_csv(r'C:\Users\user\Desktop\python Practise\New folder\companies.csv')
+df = pd.DataFrame(all_data,columns = columns_text) # making the table by matching the row and column length
+df.to_csv(r'C:\Users\user\Desktop\python Practise\New folder\companies.csv') #importing the data to my system in .CSV format.
